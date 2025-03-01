479c186ddb438356184322443526ad22522b817f
Jonathan Hitchcock <jonathan.hitchcock@discordapp.com>
2025-03-01 00:21:00 -0800

    Remove old pre-commit hook files

--- install-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make the pre-commit script executable
-chmod +x pre-commit
-
-# Create the symlink to the pre-commit hook
-ln -sf "$(pwd)/pre-commit" "$(pwd)/.git/hooks/pre-commit"
-
-echo "Git hooks installed successfully!" 
